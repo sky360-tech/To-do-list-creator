e68eeb55e3039c30750b44b07188ab72f353601d
A S M Tahmir Hasan <134987771+sky360-tech@users.noreply.github.com>
2024-02-11 08:34:51 -0600

    Add files via upload

---
+++ To Do lists.py
@@ -0,0 +1,25 @@
+thingsToDo = input("What would you like to do? ")
+
+print(thingsToDo)
+
+thingsToAdd= input("What would you like to add? ")
+
+print(thingsToDo +  thingsToAdd)
+
+thingsToRemove = input("What would you like to remove? ")
+
+print(thingsToDo + thingsToAdd + thingsToRemove)
+
+thingsToChange = input("What would you like to change? ")
+
+print(thingsToDo + thingsToAdd + thingsToRemove + thingsToChange)
+
+print("Have a nice day! You can find more information at: http://google.com")
+if thingsToDo == "list": 
+    print(thingsToDo)
+    print(thingsToAdd)
+    print(thingsToRemove)
+    print(thingsToChange)
+    print("Have a nice day! You can find more information at:")
+    print("http://google.com")
+    
